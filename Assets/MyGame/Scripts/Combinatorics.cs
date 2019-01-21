5574baa18e6fb283bf9099dc606c228b2e1f2b59
Leonhard Schnaitl <leonhard.schnaitl@htl-salzburg.ac.at>
2019-01-21 16:41:27 +0100

    made some changes to the Combinatorics.cs file

--- Assets/MyGame/Scripts/Combinatorics.cs
+++ Assets/MyGame/Scripts/Combinatorics.cs
@@ -8,18 +8,18 @@ public static class Combinatorics
 
     private const int RANDOMSEED = 123;
     //public static System.Random rnd = new System.Random(RANDOMSEED);
-    public static System.Random rnd = new System.Random();
+    public static System.Random rnd = new System.Random(); // declare need Random Object
 
     public static void Shuffle<T>(this IList<T> list)
     {
-        int n = list.Count;
+        int n = list.Count; // take the size of the list and assign the value to n
         while (n > 1)
         {
-            n--;
-            int k = rnd.Next(n + 1);
-            T value = list[k];
-            list[k] = list[n];
-            list[n] = value;
+            n--; // count down one
+            int k = rnd.Next(n + 1); // declare integer k, assign a random integer inbetween 0 and n + 1
+            T value = list[k]; // declare value with chosen datatype, assign the list k^th value to it
+            list[k] = list[n]; // assign the lists n^th value to the lists k^th value
+            list[n] = value; // assign the lists n^th value to the varible value
         }
     }
 }
